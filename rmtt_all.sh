a505a27c10154573d98e9b29bf99fd4f6c95e9b6
18162707854 <xuxiaowei0620@163.com>
2025-01-21 15:32:03 +0800

    "update"

--- rmtt_all.sh
+++ rmtt_all.sh
@@ -5,4 +5,4 @@ gnome-terminal --window -e 'bash -c "roscore; exec bash"' \
 --tab -e 'bash -c "sleep 3; roslaunch sunray_swarm orca.launch"' \
 --tab -e 'bash -c "sleep 3; cd sunray_swarm/launch_agent/; sleep 3; ./generate_launch.py -n 1; sleep 5;roslaunch sunray_swarm rmtt_all_drone.launch"' \
 --tab -e 'bash -c "sleep 3; roslaunch sunray_swarm swarm_nokov.launch"' \
---tab -e 'bash -c "sleep 3; cd ../swarmV1.0.0/; sleep 1; ./startSwarm.sh"' \
+--tab -e 'bash -c "sleep 3; cd ; sleep 1; cd swarmV1.1.0/; sleep 1; ./startSwarm.sh"' \
