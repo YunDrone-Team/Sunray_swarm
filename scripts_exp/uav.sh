43827ba4654d721e0b46a0c935f0c8b56d8be75f
yundrone <yundrone>
2025-05-12 16:55:07 +0800

    change uav.sh

--- scripts_exp/uav.sh
+++ scripts_exp/uav.sh
@@ -19,7 +19,7 @@ sleep 1
 
 read -p "请输入需要启动的rmtt数量: " num
 
-gnome-terminal --window -e "bash -c \"python3 ~/Sunray_swarm/sunray_swarm/launch_rmtt/generate_launch.py -n $num; exec bash\""
+gnome-terminal --window -e "bash -c \"cd ~/Sunray_swarm/sunray_swarm/launch_rmtt/&& python3 generate_launch.py -n $num; exec bash\""
 
 read -p "等待rmtt_all_drone.launch 生成完毕，按任意键继续..."
 
