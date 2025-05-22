85139b58d576a92b21e8b1915a73166186931bd7
yundrone <yundrone>
2025-05-22 19:08:45 +0800

    change auto_start_ugv

--- auto_start_ugv.sh
+++ auto_start_ugv.sh
@@ -3,9 +3,9 @@
 ## 虽然环境变量里面已经做了如下设置，但是设置为开机自启动脚本的时候，系统还没有完全启动，所以需要手动source
 source /opt/ros/noetic/setup.bash
 source ~/Sunray_swarm/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.25.218:11311
-export ROS_HOSTNAME=192.168.25.81
-export ROS_IP=192.168.25.81
+export ROS_MASTER_URI=http://192.168.25.91:11311
+export ROS_HOSTNAME=192.168.25.162
+export ROS_IP=192.168.25.162
 
 # 等待开发主机中的roscore就绪
 echo "等待roscore启动..."
@@ -31,13 +31,16 @@ fi
 
 ## 不同的无人车注意修改下面参数中的ugv_id
 ## 启动wheeltec_driver
+sleep 5
 gnome-terminal --window --title="wheeltec_driver" -- bash -c "roslaunch sunray_swarm wheeltec_driver.launch ugv_id:=1; exec bash"
+sleep 5
 ## 启动tianbot_driver
 ##gnome-terminal --window --title="tianbot_driver" -- bash -c "roslaunch sunray_swarm tianbot_driver.launch ugv_id:=1; exec bash"
-sleep 1
 ## 启动ugv_control_node
 gnome-terminal --window --title="ugv_control_node" -- bash -c "roslaunch sunray_swarm ugv_control_node.launch ugv_id:=1; exec bash"
 ## 启动ugv_control_node_diff
 ##gnome-terminal --window --title="ugv_control_node" -- bash -c "roslaunch sunray_swarm ugv_control_node_diff.launch ugv_id:=1; exec bash"
-
+##启动ugv_cam
+sleep 5
+gnome-terminal --window --title="ugv_cam" -- bash -c "roslaunch sunray_swarm ugv_camera.launch ugv_id:=1; exec bash"
 
